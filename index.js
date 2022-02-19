eabc0d1e5d5b3b626b6a03ede2db1716f9bb8c18
Nikita Logunyonok <38005134+zeVACa@users.noreply.github.com>
2022-02-19 23:10:12 +0300

    first task complete

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const app = express();
 
-// const server = http.createServer((req, res) => {
-//   if (req.method === 'GET') {
-//     res.end(`
-//       <input type="text" />
-//     `);
-//   }
-// });
+const urlencodedParser = express.urlencoded({ extended: false });
 
 app.get('/', (req, res) => {
   res.send(`
   <form action="http://localhost:3000/login" method="post">
-  <label>Логин</label> <input type="text" name="login"><br><br>
-  <label>Пароль</label> <input type="password" name="password"><br><br>
-  <button type="submit">отправить</button>
-   </form>
+        <label>Логин</label><br>
+        <input type="text" name="login" /><br><br>
+        <label>Пароль</label><br>
+        <input type="password" name="password" /><br><br>
+        <input type="submit" value="Отправить" />
+    </form>
   `);
 });
 
-app.post('/login', (req, res) => {
-  console.log(req);
+const users = [
+  {
+    login: 'admin',
+    password: 'password',
+  },
+];
+
+app.post('/login', urlencodedParser, (req, res) => {
+  if (users.find((user) => user.login === req.body.login && user.password === req.body.password)) {
+    res.status(200).send(`<h1>Пользователь с логином "${req.body.login}" успешно авторизован</h1>`);
+  } else {
+    res.status(401).send(`
+      <form action="http://localhost:3000/login" method="post">
+          <label>Логин</label><br>
+          <input type="text" name="login" /><br><br>
+          <label>Пароль</label><br>
+          <input type="password" name="password" /><br><br>
+          <input type="submit" value="Отправить" />
+          <h1>Неправильный логин или пароль</h1>
+      </form>
+    `);
+  }
   res.send('POST response');
 });
 
