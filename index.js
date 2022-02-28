5c204340be1770cc931e9bf7ce87ffc1190ee39b
Nikita Logunyonok <38005134+zeVACa@users.noreply.github.com>
2022-02-28 15:25:49 +0300

    prevent default submit

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const app = express();
 const urlencodedParser = express.urlencoded({ extended: false });
 
 app.get('/', (req, res) => {
-  res.send(`
-  <form action="http://localhost:3000/login" method="post">
-        <label>Логин</label><br>
-        <input type="text" name="login" /><br><br>
-        <label>Пароль</label><br>
-        <input type="password" name="password" /><br><br>
-        <input type="submit" value="Отправить" />
-    </form>
-  `);
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/client.js', (req, res) => {
+  console.log('client.js has been requested');
+  res.sendFile(__dirname + '/client.js');
 });
 
 const users = [
@@ -22,22 +19,22 @@ const users = [
   },
 ];
 
-app.post('/login', urlencodedParser, (req, res) => {
-  if (users.find((user) => user.login === req.body.login && user.password === req.body.password)) {
-    res.status(200).send(`<h1>Пользователь с логином "${req.body.login}" успешно авторизован</h1>`);
-  } else {
-    res.status(401).send(`
-      <form action="http://localhost:3000/login" method="post">
-          <label>Логин</label><br>
-          <input type="text" name="login" /><br><br>
-          <label>Пароль</label><br>
-          <input type="password" name="password" /><br><br>
-          <input type="submit" value="Отправить" />
-          <h1>Неправильный логин или пароль</h1>
-      </form>
-    `);
-  }
-  res.send('POST response');
-});
+// app.post('/login', urlencodedParser, (req, res) => {
+//   if (users.find((user) => user.login === req.body.login && user.password === req.body.password)) {
+//     res.status(200).send(`<h1>Пользователь с логином "${req.body.login}" успешно авторизован</h1>`);
+//   } else {
+//     res.status(401).send(`
+//       <form action="http://localhost:3000/login" method="post">
+//           <label>Логин</label><br>
+//           <input type="text" name="login" /><br><br>
+//           <label>Пароль</label><br>
+//           <input type="password" name="password" /><br><br>
+//           <input type="submit" value="Отправить" />
+//           <h1>Неправильный логин или пароль</h1>
+//       </form>
+//     `);
+//   }
+//   res.send('POST response');
+// });
 
 app.listen(3000, () => console.log('server is running...'));
